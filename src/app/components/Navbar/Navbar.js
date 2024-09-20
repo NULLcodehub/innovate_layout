c277ca92f58d16c4993f2bb639750fa420a013d6
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-09-20 14:26:17 +0600

    search bar added to teh navbar

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import { GoChevronDown } from "react-icons/go";
+import { FaSearch } from 'react-icons/fa';
+import { GoMultiSelect } from "react-icons/go";
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <>
-            <section>
-                <div>
-                    <div className='nav-list'>
-                            <ul>
-                                <li className='nav-item'></li>
-                                <li className='nav-item'></li>
-                                <li className='nav-item'></li>
-                                <li className='nav-item'></li>
-                            </ul>
-                    </div>
+      <>
+        <section className="Navbar flex justify-around items-center">
+          <div className="nav-list hidden lg:flex">
+            <ul className="flex gap-16">
+              <li className="nav-item">
+                Home <GoChevronDown />{" "}
+              </li>
+              <li className="nav-item">
+                Event <GoChevronDown />
+              </li>
+              <li className="nav-item">
+                Contact <GoChevronDown />
+              </li>
+              <li className="nav-item">
+                Blog <GoChevronDown />
+              </li>
+            </ul>
+          </div>
 
-                    <div className='searchbar'>
+          <div className="flex items-center border rounded-full overflow-hidden search ">
 
-                    </div>
-                </div>
-            </section>
-            
-        </>
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 p-2 outline-none border-none"
+            />
+            <span className="p-2 text-white ">
+                <FaSearch className=''/>
+            </span>
+          </div>
+
+          <div className='block lg:hidden'>
+            <GoMultiSelect className='text-white size-8'/>
+          </div>
+        </section>
+      </>
     );
 };
 
