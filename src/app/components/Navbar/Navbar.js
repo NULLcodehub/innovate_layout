78544addd21d4be0a980d8bf8c08627f25fbe7c8
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-09-20 13:40:18 +0600

    navbar added

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <div className='nav-list'>
+                            <ul>
+                                <li className='nav-item'></li>
+                                <li className='nav-item'></li>
+                                <li className='nav-item'></li>
+                                <li className='nav-item'></li>
+                            </ul>
+                    </div>
+
+                    <div className='searchbar'>
+
+                    </div>
+                </div>
+            </section>
+            
+        </>
+    );
+};
+
+export default Navbar;
